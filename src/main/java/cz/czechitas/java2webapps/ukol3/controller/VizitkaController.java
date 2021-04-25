6c6c7923d2840b16912663e2ba085399d52e037a
dana-holubova <danmich@email.cz>
2021-04-25 08:10:56 +0200

    Dynamické generování vizitek

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,49 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
-  @GetMapping("/")
-  public String seznam() {
-    return "seznam";
-  }
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
-  }
+    //  field vizitky
+    private final List<Vizitka> vizitky;
+
+    //  konstruktor
+    public VizitkaController() {
+        vizitky = Arrays.asList(
+                new Vizitka("Dana Holubová", "Masarykova univerzita", "Žerotínovo náměstí 617/9",
+                        "Brno", "60200", "danmich@email.cz", "+420 124587", "www.botanickafotogalerie.cz"),
+                new Vizitka("Nikola Holubová", "IPC", "Šumavská 416/15",
+                        "Brno","60200",null, "+420 800123456", "https://www.autocont-ipc.cz/"),
+                new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Václavské náměstí 837/11",
+                        "Praha 1", "11000", "monika@czechitas.cs", "+420 800123456", null),
+                new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Václavské náměstí 837/11",
+                        "Praha 1", "11000", "mirka@czechitas.cs", null, "www.czechitas.cz")
+        );
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+ModelAndView result = new ModelAndView("seznam");
+result.addObject("vizitky", vizitky);
+        return result;
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detail(int id) {
+        ModelAndView result = new ModelAndView("detail");
+        result.addObject("vizitka", vizitky.get(id));
+        return result;
+    }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -0,0 +1,122 @@
+package cz.czechitas.java2webapps.ukol3.entity;
+
+public class Vizitka {
+    //fieldy
+    private String jmeno;
+    private String firma;
+    private String ulice;
+    //    obec a psc jsem udělala zvlášť, protože se mi to hodí pro zobrazování adresy na mapě
+//    zobrazení na mapě totiž nefunguje, když se tam dá psč
+    private String obec;
+    private String psc;
+    private String email;
+    private String telefon;
+    private String web;
+//    odvozené fieldy
+    private String obecPsc;
+    private String celaAdresa;
+    private String uliceObec;
+
+//bezparametrový konstruktor
+
+
+    public Vizitka() {
+    }
+
+//konstruktor se všemi fieldy kromě obecPsc, celaAdresa a uliceObec
+
+    public Vizitka(String jmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
+        this.jmeno = jmeno;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.obec = obec;
+        this.psc = psc;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+
+//      odvozené fieldy
+        obecPsc = obec + " " + psc;
+        celaAdresa = ulice + ", " + obec;
+        uliceObec = ulice + ", " + obec;
+    }
+
+//    gettry a settry
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getObec() {
+        return obec;
+    }
+
+    public void setObec(String obec) {
+        this.obec = obec;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+//    jen gettry pro fieldy určené jen pro čtení
+    public String getObecPsc() {
+        return obecPsc;
+    }
+
+    public String getCelaAdresa() {
+        return celaAdresa;
+    }
+
+    public String getUliceObec() {
+        return uliceObec;
+    }
+
+}
